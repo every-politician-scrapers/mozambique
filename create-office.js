09a274292b8978efc41212787639f4ab979808f4
Tony Bowden <tony@tmtm.com>
2022-04-28 11:01:33 +0300

    update to latest wikibase-cli scripts

--- create-office.js
+++ create-office.js
@@ -3,23 +3,21 @@ const fs = require('fs');
 let rawmeta = fs.readFileSync('meta.json');
 let meta = JSON.parse(rawmeta);
 
-module.exports = (enlabel, ptlabel) => {
+module.exports = (label, ministry) => {
   claims = {
     P31:   { value: 'Q294414' }, // instance of: public office
     P279:  { value: 'Q83307'  }, // subclas of: minister
     P17:   { value: meta.country ? meta.country.id : meta.jurisdiction.id },
     P1001: { value: meta.jurisdiction.id },
-    P361: { // part of
-      value: meta.cabinet.parent,
-      references: { P854: meta.source.url },
-    }
+    P361:  { value: meta.cabinet.parent }
   }
+
+  if(ministry)   claims['P2389']  = ministry
 
   return {
     type: 'item',
     labels: {
-      en: enlabel,
-      pt: ptlabel,
+      en: label,
     },
     descriptions: {
       en: `cabinet position in ${meta.jurisdiction.name}`,
