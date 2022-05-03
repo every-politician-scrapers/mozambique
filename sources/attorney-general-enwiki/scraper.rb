0762fb3c0a22411363605a87b45971b6e6111b0f
Tony Bowden <tony@tmtm.com>
2022-05-03 11:53:29 +0300

    compare enwiki Attorneys General

---
+++ sources/attorney-general-enwiki/scraper.rb
@@ -0,0 +1,56 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderNonTable < OfficeholderListBase::OfficeholderBase
+  def empty?
+    false
+  end
+
+  def combo_date?
+    true
+  end
+
+  def raw_combo_date
+    raise 'need to define a raw_combo_date'
+  end
+
+  def name_node
+    raise 'need to define a name_node'
+  end
+end
+
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  # decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  # TODO: make this easier to override
+  def holder_entries
+    noko.xpath("//h2[.//span[contains(.,'attorneys general')]][last()]//following-sibling::ul[1]//li[a]")
+  end
+
+  class Officeholder < OfficeholderNonTable
+    def raw_combo_date
+      noko.text.tidy.split(',').last.gsub('.', '')
+    end
+
+    def name_node
+      noko.css('a').first
+    end
+
+    def item
+      noko.css('a/@wikidata').map(&:text).first
+    end
+
+    def empty?
+      super || noko.text.include?('?')
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
