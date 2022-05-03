7fd8111a8b9be6e029217461d5b43bcc5dbf7b41
Tony Bowden <tony@tmtm.com>
2022-05-03 10:16:26 +0300

    compare enwiki Foreign Ministers

---
+++ sources/minister-foreign/scraper.rb
@@ -0,0 +1,53 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/scraper_data'
+require 'pry'
+
+class OfficeholderNonTable < OfficeholderListBase::OfficeholderBase
+  def empty?
+    false
+  end
+
+  def combo_date?
+    true
+  end
+
+  def raw_combo_date
+    raise 'need to define a raw_combo_date'
+  end
+
+  def name_node
+    raise 'need to define a name_node'
+  end
+end
+
+
+class OfficeholderList < OfficeholderListBase
+  decorator RemoveReferences
+  # decorator UnspanAllTables
+  decorator WikidataIdsDecorator::Links
+
+  # TODO: make this easier to override
+  def holder_entries
+    noko.xpath("//h2[.//span[contains(.,'List of ministers')]][last()]//following-sibling::ul[1]//li[a]")
+  end
+
+  class Officeholder < OfficeholderNonTable
+    # TODO: push up
+    def raw_combo_date
+      noko.text.split(':', 2).first.tidy
+    end
+
+    def name_node
+      noko.css('a').first
+    end
+
+    def item
+      noko.css('a/@wikidata').map(&:text).first
+    end
+  end
+end
+
+url = ARGV.first
+puts EveryPoliticianScraper::ScraperData.new(url, klass: OfficeholderList).csv
