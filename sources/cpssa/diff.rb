00fac6b80b636a47143d0da0dd23bbaebf51e701
Tony Bowden <tony@tmtm.com>
2022-05-04 13:01:37 +0300

    Remap 2019-12-31 to 2020-01-17
    The external data only goes up to the end of 2019, but the cabinet
    continued until 2020-01-17, so treat anything still open at the end of
    2019 as continuing until the end of the term. It's remotely possible
    that a position may have terminated in-between those, but that can
    be corrected manually if we come across any.

--- sources/cpssa/diff.rb
+++ sources/cpssa/diff.rb
@@ -3,5 +3,11 @@
 
 require 'every_politician_scraper/comparison'
 
-diff = EveryPoliticianScraper::Comparison.new('wikidata.csv', 'scraped.csv').diff
+class Comparison < EveryPoliticianScraper::NulllessComparison
+  def external_csv_options
+    { converters: [->(v) { v.to_s.sub('2019-12-31', '2020-01-17') }] }
+  end
+end
+
+diff = Comparison.new('wikidata.csv', 'scraped.csv').diff
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
