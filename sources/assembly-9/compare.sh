74bf39249e3c3808df76853aee08bdd29858105e
Tony Bowden <tony@tmtm.com>
2022-05-05 16:12:06 +0300

    refresh diff

--- sources/assembly-9/compare.sh
+++ sources/assembly-9/compare.sh
@@ -3,7 +3,7 @@
 cd $(dirname $0)
 
 # Original extracted from https://www.portaldogoverno.gov.mz/por/Imprensa/Noticias/Quem-sao-os-deputados-da-AR
-qsv select name,party,constituency raw.csv | qsv join --left party - partyLabel parties.csv | qsv join --left constituency - provinceLabel provinces.csv | qsv select name,party\[1\],partyLabel,province,constituency | qsv rename name,party,partyLabel,area,areaLabel > scraped.csv
+qsv select name,party,constituency raw.csv | qsv join --left party - partyLabel parties.csv | qsv join --left constituency - provinceLabel provinces.csv | sed -e 's/$/,/' | qsv select "8,name,party[1],partyLabel,province,constituency" | qsv rename item,name,party,partyLabel,area,areaLabel > scraped.csv
 
 wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv sort -s name,startDate > wikidata.csv
 
