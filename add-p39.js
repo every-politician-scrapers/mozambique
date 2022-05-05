4d3cbc0ce0aee1ce2a53748a220336e38fadb305
Tony Bowden <tony@tmtm.com>
2022-05-05 12:24:45 +0300

    update add-p39.js

--- add-p39.js
+++ add-p39.js
@@ -6,20 +6,19 @@ module.exports = (id, position, startdate, enddate) => {
   qualifier = {
     P580: meta.cabinet.start,
   }
+  if(startdate) qualifier['P580']  = startdate
+  if(enddate)   qualifier['P582']  = enddate
 
-  refs = { }
-
+  reference = { }
   if(process.env.REF) {
     var wpref = /wikipedia.org/;
     if (wpref.test(process.env.REF)) {
-      refs['P4656'] = process.env.REF
+      reference['P4656'] = process.env.REF
     } else {
-      refs['P854'] = process.env.REF
+      reference['P854'] = process.env.REF
     }
   }
-
-  if(startdate) qualifier['P580']  = startdate
-  if(enddate)   qualifier['P582']  = enddate
+  reference['P813'] = new Date().toISOString().split('T')[0]
 
   return {
     id,
@@ -27,7 +26,7 @@ module.exports = (id, position, startdate, enddate) => {
       P39: {
         value: position,
         qualifiers: qualifier,
-        references: refs
+        references: reference,
       }
     }
   }
