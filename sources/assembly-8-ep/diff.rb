eea4f1aa163d7981539e133bec92758da672bd24
Tony Bowden <tony@tmtm.com>
2022-05-05 15:58:21 +0300

    correct casing for EP 8
    Undo the bad capitalisation that had been left in by accident.

--- sources/assembly-8-ep/diff.rb
+++ sources/assembly-8-ep/diff.rb
@@ -3,24 +3,9 @@
 
 require 'every_politician_scraper/comparison'
 
-# from https://stackoverflow.com/questions/1791639/converting-upper-case-string-into-title-case-using-ruby
-class String
-  def titlecase
-    split(/([[:alpha:]]+)/).map(&:capitalize).join
-  end
-end
-
 class Comparison < EveryPoliticianScraper::NulllessComparison
   def columns
     super - %i(arealabel partylabel)
-  end
-
-  def wikidata_csv_options
-    { converters: [->(val, field) { field.header == :name ? val.to_s.titlecase : val }] }
-  end
-
-  def external_csv_options
-    { converters: [->(val, field) { field.header == :name ? val.to_s.titlecase : val.to_s.gsub('Q3366357', 'Q953447') }] }
   end
 end
 
