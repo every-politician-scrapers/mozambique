3a99c3d333b19163c5660c8a2d59b645f467ce45
Tony Bowden <tony@tmtm.com>
2022-05-05 14:42:50 +0300

    Migrate Renamo-UE to RENAMO
    Renamo-UE existed for the 2004 elections, but was dissolved in 2007. I
    assume this was a mis-reconciliation within EP, as the source there[1]
    simply lists RENAMO. So convert this en passant to the main RENAMO Qid.
    
    I also fixed the data already submitted to Wikidata using `wd uq`[2]
    
    [1] https://github.com/everypolitician/everypolitician-data/blob/master/data/Mozambique/Assembly/sources/morph/official.csv
    
    [2] https://tools.wmflabs.org/editgroups/b/wikibase-cli/6127d2c994a7c

--- sources/assembly-8-ep/compare.sh
+++ sources/assembly-8-ep/compare.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 
-# curl https://raw.githubusercontent.com/everypolitician/everypolitician-data/master/data/Mozambique/Assembly/term-8.csv | qsv select wikidata,name,wikidata_group,group,wikidata_area,area,start_date,end_date,gender | qsv rename item,name,party,partyLabel,area,areaLabel,startDate,endDate,gender > scraped.csv
+# curl https://raw.githubusercontent.com/everypolitician/everypolitician-data/master/data/Mozambique/Assembly/term-8.csv | qsv select wikidata,name,wikidata_group,group,wikidata_area,area,start_date,end_date,gender | sed -e 's/Q1740056/Q1425821/g' | qsv rename item,name,party,partyLabel,area,areaLabel,startDate,endDate,gender > scraped.csv
 
 wd sparql -f csv wikidata.js | sed -e 's/T00:00:00Z//g' -e 's#http://www.wikidata.org/entity/##g' | qsv dedup -s psid | qsv sort -s name,startDate > wikidata.csv
 bundle exec ruby diff.rb | tee diff.csv
