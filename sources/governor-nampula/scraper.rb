9aac599073e389b4e333c5eac1bf81b576c8e7e9
Tony Bowden <tony@tmtm.com>
2022-06-17 14:48:50 +0300

    update to latest wpoffice

--- sources/governor-nampula/scraper.rb
+++ sources/governor-nampula/scraper.rb
@@ -4,40 +4,15 @@
 require 'every_politician_scraper/scraper_data'
 require 'pry'
 
-class OfficeholderNonTable < OfficeholderListBase::OfficeholderBase
-  def empty?
-    too_early?
-  end
-
-  def combo_date?
-    true
-  end
-
-  def raw_combo_date
-    raise 'need to define a raw_combo_date'
-  end
-
-  def name_node
-    raise 'need to define a name_node'
-  end
-end
-
-
 class OfficeholderList < OfficeholderListBase
   decorator RemoveReferences
-  # decorator UnspanAllTables
   decorator WikidataIdsDecorator::Links
 
-  # TODO: make this easier to override
   def holder_entries
     noko.xpath("//h3[.//span[contains(.,'Governadores')]]//following-sibling::ul[1]//li")
   end
 
-  class Officeholder < OfficeholderNonTable
-    def raw_combo_date
-      noko.text.gsub('-)', '- Incumbent)')[/\((.*?)\)/, 1]
-    end
-
+  class Officeholder < OfficeholderNonTableBase
     def itemLabel
       noko.text.split(')', 2).last.tidy
     end
@@ -46,8 +21,9 @@ class OfficeholderList < OfficeholderListBase
       noko.css('a/@wikidata').map(&:text).last
     end
 
-    def ignore_before
-      1995
+    def raw_combo_date
+      years = noko.text.gsub('-)', '- Incumbent)')[/\((.*?)\)/, 1]
+      years =~ /^\d{4}$/ ? "#{years} - #{years}" : years
     end
   end
 end
