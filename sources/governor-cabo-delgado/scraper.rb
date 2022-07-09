8b018bb541dbfd01af051dc8f9096b5d6e924ba2
Tony Bowden <tony@tmtm.com>
2022-07-09 10:55:26 +0300

    upgrade scraper

--- sources/governor-cabo-delgado/scraper.rb
+++ sources/governor-cabo-delgado/scraper.rb
@@ -4,54 +4,25 @@
 require 'every_politician_scraper/scraper_data'
 require 'pry'
 
-class OfficeholderNonTable < OfficeholderListBase::OfficeholderBase
-  def empty?
-    too_early?
-  end
-
-  def combo_date?
-    true
-  end
-
-  def raw_combo_date
-    raise 'need to define a raw_combo_date'
-  end
-
-  def name_node
-    raise 'need to define a name_node'
-  end
-end
-
-
 class OfficeholderList < OfficeholderListBase
   decorator RemoveReferences
-  # decorator UnspanAllTables
   decorator WikidataIdsDecorator::Links
 
-  # TODO: make this easier to override
   def holder_entries
     noko.xpath("//h4[.//span[contains(.,'Governadores')]]//following-sibling::ul[1]//li")
   end
 
-  class Officeholder < OfficeholderNonTable
-    def raw_combo_date
-      noko.text.gsub('-)', '- Incumbent)')[/\((.*?)\)/, 1]
-    end
-
-    def itemLabel
-      noko.text.split(')', 2).last.tidy
-    end
-
+  class Officeholder < OfficeholderNonTableBase
     def item
       noko.css('a/@wikidata').map(&:text).last
     end
 
-    def empty?
-      noko.text.include?('?') || super
+    def itemLabel
+      noko.text.split(')').last.tidy
     end
 
-    def ignore_before
-      1997
+    def raw_combo_date
+      noko.text.gsub('-)', '- Incumbent)')[/\((.*?)\)/, 1]
     end
   end
 end
